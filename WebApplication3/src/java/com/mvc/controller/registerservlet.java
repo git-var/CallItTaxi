37b42d223599050b330052b57856e9cad95b74f5
git-kiran <kiransidhartha7@gmail.com>
2016-11-08 15:59:54 +0000

    Add files via upload

--- WebApplication3/src/java/com/mvc/controller/registerservlet.java
+++ WebApplication3/src/java/com/mvc/controller/registerservlet.java
@@ -5,6 +5,11 @@
  */
 package com.mvc.controller;
 import com.mvc.people.User;
+import com.mvc.people.customertype;
+import com.mvc.people.customertype.categoryinterface;
+import com.mvc.people.student;
+import com.mvc.people.userFactory;
+import com.mvc.people.worker;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -47,14 +52,41 @@ public class registerservlet extends HttpServlet {
             throws ServletException, IOException 
         {                
                   //SingleObject object = SingleObject.getInstance();
-   //     User user = User.getInstance();                
-User user = new User();
-                user.setUsername(request.getParameter("username"));
-                user.setPassword(request.getParameter("password"));
-                user.setEmail(request.getParameter("email"));
-                user.setAddress(request.getParameter("address"));
-                user.setPhno(request.getParameter("phno"));
-                user.setCategory(request.getParameter("category"));
+   //     User user = User.getInstance(); 
+String username,password,email,address,phno,category;
+float discount = 0;
+            username=request.getParameter("username");
+                password=request.getParameter("password");
+                email=request.getParameter("email");
+                address=request.getParameter("address");
+                phno=request.getParameter("phno");
+                category=request.getParameter("category"); 
+                
+              
+            //    userFactory uf=new userFactory();    
+      // student us;
+       //us = (student) uf.getuser(category);
+       //worker ws=(worker) uf.getuser(category);
+    categoryinterface ci=   userFactory.getuser(category);
+    ci.setDiscount();
+    discount=ci.getDiscount();
+//    if(us!=null)
+//       {         us.setDiscount();
+//       discount=us.getDiscount();
+//       }
+//     else{
+//          ws.setDiscount();
+//          discount=ws.getDiscount();
+//                   }
+       User user=new User();
+//User user = new User();
+                user.setUsername(username);
+                user.setPassword(password);
+                user.setEmail(email);
+                user.setAddress(address);
+                user.setPhno(phno);
+                      user.setCategory(category);
+                user.setDiscount(discount);
 
        try {
            user.RegisterUser();
